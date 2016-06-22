54a0621c5232b20c20660fd041985261b7efc082
kr <admin@krdemac-mini.local>
2016-06-22 11:37:51 +0800

    Initial Commit

---
+++ Swift 功能封装/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift 功能封装
+//
+//  Created by kr on 16/6/22.
+//  Copyright © 2016年 凯如科技. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
